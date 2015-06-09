5e1ef013b536eca900502092ee14d1374bb72317
Matheus Kautzmann <kautzmann5@gmail.com>
2015-06-09 15:24:05 -0300

    Check if brew is already installed, else install it first

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,13 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+	if [ "$(which brew)" = "brew not found" ]; then
+		if [ $(which ruby) = "ruby not found" ] || [ "$(which curl)" = "curl not found" ]; then
+			echo "You must install Ruby and cURL before running this script!";
+		else
+			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+		fi;
+	fi;
 	source ~/.bash_profile;
 }
 
