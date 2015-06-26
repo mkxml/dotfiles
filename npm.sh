8e179923892af3c808c3e026b7881d6c3a8c5052
Matheus Kautzmann <kautzmann5@gmail.com>
2015-06-26 13:58:56 -0300

    Add important Node.JS global modules

---
+++ npm.sh
@@ -0,0 +1,19 @@
+# Install useful Node.JS modules globally
+
+sudo -v
+
+sudo npm install bower
+sudo npm install coffee-script
+sudo npm install coffeelint
+sudo npm install csslint
+sudo npm install docpad
+sudo npm install express
+sudo npm install grunt-cli
+sudo npm install htmlhint
+sudo npm install jshint
+sudo npm install mocha
+sudo npm install plato
+sudo npm install supervisor
+sudo npm install uglify-js
+sudo npm install vtop
+sudo npm install yo
