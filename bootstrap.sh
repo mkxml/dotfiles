58cb36f7b540d0e89ac5e7632dbabf8202943ff6
Matheus Kautzmann <kautzmann5@gmail.com>
2015-06-30 13:37:56 -0300

    Check brew on Bash and ZSH

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,8 @@ git pull origin master;
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	if [ "$(which brew)" = "brew not found" ]; then
-		if [ $(which ruby) = "ruby not found" ] || [ "$(which curl)" = "curl not found" ]; then
-			echo "You must install Ruby and cURL before running this script!";
-		else
-			ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		fi;
+	if [ "$(which brew)" = "brew not found" ] || ["$(which brew)" = ""]; then
+		ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi;
 	source ~/.bash_profile;
 }
