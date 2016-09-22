b219f143ce334dea3f5e1958f0a14b0b6c50cc2d
Matheus Kautzmann <kautzmann5@gmail.com>
2016-09-22 10:58:55 -0300

    Add OneDark theme for vim-airline

--- vim.sh
+++ vim.sh
@@ -4,5 +4,8 @@
 # Install Vundle plugins before running Vim the first time
 vim +PluginInstall +qall
 
+# Install vim-airline onedark theme
+cp ~/themes/vim-airline/onedark.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/onedark.vim
+
 # Compile YouCompleteMe plugin
 ~/.vim/bundle/YouCompleteMe/install.py --cland-completer --tern-completer
