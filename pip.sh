ece26229d3a042a605ac7be78a3fbef87c95497e
Matheus Kautzmann <kautzmann5@gmail.com>
2017-07-27 20:32:05 -0300

    Change aws-cli tool to awscli

--- pip.sh
+++ pip.sh
@@ -6,7 +6,7 @@ sudo -v
 sudo easy_install pip
 
 # Install some tooling with PIP
-pip install --upgrade aws-cli
+pip install --upgrade awscli
 pip install --upgrade cppclient
 pip install --upgrade httpie
 pip install --upgrade virtualenv
