d2e53275a69e59ac373f98ec9db335cd07c7721e
Matheus Kautzmann <kautzmann5@gmail.com>
2017-07-28 11:20:20 -0300

    Clean up npm scripts

--- npm.sh
+++ npm.sh
@@ -1,24 +1,16 @@
 # Install useful Node.JS modules globally
 
 npm install -g babel
-npm install -g bower
-npm install -g browserify
 npm install -g coffee-script
-npm install -g coffeelint
 npm install -g cordova
 npm install -g csslint
+npm install -g eslint
 npm install -g docpad
 npm install -g express
-npm install -g grunt-cli
-npm install -g htmlhint
-npm install -g jshint
 npm install -g karma-cli
 npm install -g mocha
 npm install -g npm-which
-npm install -g plato
 npm install -g react-native-cli
-npm install -g supervisor
 npm install -g uglify-js
 npm install -g vtop
 npm install -g webpack
-npm install -g yo
