7bb38c460dc4b39339e09750d34cd310c6bbd60b
Matheus Kautzmann <kautzmann5@gmail.com>
2017-07-28 11:26:39 -0300

    Add node install config with nvm

---
+++ nvm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Check whether we already have a node installation
+NODE_PATH=$(which node)
+
+if [ "$NODE_PATH" = "" ]; then
+  # First node installation
+  # Just get latest node.js and npm
+  nvm install node
+else
+  # Not the first node installation
+  # Get node version before update
+  PREVIOUS_NODE_VERSION=$(node -v)
+  # Get latest node.js and npm
+  nvm install node
+  # Reinstall packages from previous version
+  nvm reinstall-packages $PREVIOUS_NODE_VERSION
+fi
+
+# Alias default to latest node
+nvm alias default node
+
+echo "Done! Please restart your terminal session."
