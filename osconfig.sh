67904d9a60434583f9243054c2b948c1f48600ad
Matheus Kautzmann <mkvchain@gmail.com>
2018-01-05 17:08:22 -0200

    Map Caps-Lock to Control key

--- osconfig.sh
+++ osconfig.sh
@@ -516,6 +516,30 @@ sudo mdutil -i on / > /dev/null
 sudo mdutil -E / > /dev/null
 
 ###############################################################################
+# Keyboard config                                                             #
+###############################################################################
+
+# Map Caps-Lock to Ctrl
+osascript <<EOD
+
+tell application "System Preferences"
+    reveal anchor "keyboardTab" of pane "com.apple.preference.keyboard"
+end tell
+tell application "System Events" to tell window 1 of process "System Preferences"
+    click button "Modifier Keys…" of tab group 1
+    tell sheet 1
+        tell pop up button 1
+            click
+            click menu item "^ Control" of menu 1
+        end tell
+        click button "OK"
+    end tell
+end tell
+quit application "System Preferences"
+
+EOD
+
+###############################################################################
 # Terminal                                                                    #
 ###############################################################################
 
