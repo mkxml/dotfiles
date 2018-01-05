734a542bad81dc0e5e637998f77eb14361ca5bce
Matheus Kautzmann <mkvchain@gmail.com>
2018-01-05 14:32:19 -0200

    Add automatic install for Mac App Store apps with mas

---
+++ mas.sh
@@ -0,0 +1,19 @@
+# Personal account, may change it
+mas signin matheusrks@hotmail.com
+
+mas install 421131143 # MPlayerX
+mas install 451444120 # Memory Clean
+mas install 425264550 # Blackmagic Disk Speed Test
+mas install 407963104 # Pixelmator
+mas install 497799835 # Xcode
+mas install 404010395 # TextWrangler
+mas install 585829637 # Todoist
+mas install 557168941 # Tweetbot
+mas install 787259510 # Favicon Creator
+mas install 727593140 # VPN Unlimited
+mas install 668208984 # GIPHY Capture
+mas install 1037126344 # Apple Configurator 2
+mas install 1176895641 # Spark mail
+mas install 425424353 # The unarchiver
+
+mas upgrade
