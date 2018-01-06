9685681f2b4161ccf02953cd2f96aa9e49aae4e5
Matheus Kautzmann <mkvchain@gmail.com>
2018-01-06 10:34:48 -0200

    Fix .keyboard

---
+++ .keyboard/hammerspoon/hyper-apps-defaults.lua
@@ -0,0 +1,17 @@
+-- Default keybindings for launching apps in Hyper Mode
+--
+-- To launch _your_ most commonly-used apps via Hyper Mode, create a copy of
+-- this file, save it as `hyper-apps.lua`, and edit the table below to configure
+-- your preferred shortcuts.
+return {
+  { 'b', 'Safari' },
+  { 'c', 'Slack' },
+  { 'd', 'Todoist' },
+  { 'e', 'Visual Studio Code' },
+  { 'f', 'Finder' },
+  { 'g', 'Google Chrome' },
+  { 'i', 'iTunes' },
+  { 'm', 'Spark' },
+  { 's', 'Spotify' },
+  { 't', 'Terminal' },
+}
