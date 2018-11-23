0ce3ec6fac8c1911b9a23024ce08bd0e1a41dc49
Matheus Kautzmann <mkvchain@gmail.com>
2018-11-23 10:06:38 -0200

    Install less node stuff globally

--- npm.sh
+++ npm.sh
@@ -2,17 +2,12 @@
 
 # Install useful Node.JS modules globally
 
-npm install -g babel
-npm install -g coffee-script
 npm install -g cordova
 npm install -g csslint
 npm install -g eslint
 npm install -g docpad
 npm install -g express
-npm install -g karma-cli
-npm install -g mocha
 npm install -g npm-which
 npm install -g react-native-cli
 npm install -g uglify-js
 npm install -g vtop
-npm install -g webpack
